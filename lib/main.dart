41968e71d3c7f78310aec4ce3bf4c7588adadc9f
hassibhajj <hassibhajj5@gmail.com>
2025-11-20 21:58:32 +0200

    created root widget for the app and the water intake page

--- lib/main.dart
+++ lib/main.dart
@@ -9,17 +9,34 @@ class HydroBuddyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return  MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'HydroBuddy',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('HydroBuddy'),
-        ),
-        body: const Center(
-          child: Text('Welcome to HydroBuddy!'),
-        ),
-      ),
+      title: "HydroBuddy",
+      home: const WaterIntakePage(),
     );
   }
 }
+
+class WaterIntakePage extends StatefulWidget {
+  const WaterIntakePage({super.key});
+
+  @override
+  State<WaterIntakePage> createState() => _WaterIntakePageState();
+}
+
+class _WaterIntakePageState extends State<WaterIntakePage> {
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      appBar: AppBar(
+        title: const Text("HydroBuddy - Water Intake"),
+      ),
+        body: const Center(
+          child: Text(" lets calculate your daily water intake"),
+        ),
+
+    );
+  }
+}
+
+
